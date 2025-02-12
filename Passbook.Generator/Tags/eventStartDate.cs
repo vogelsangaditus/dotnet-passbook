ef7daecb06c85adb2c61ff3b2906be050f301371
Daniel Vogelsang <d.vogelsang@aditus.de>
2025-02-12 07:41:28 +0100

    Add DateTimeOffset overload to EventStartDate and EventEndDate

--- Passbook.Generator/Tags/eventStartDate.cs
+++ Passbook.Generator/Tags/eventStartDate.cs
@@ -1,9 +1,22 @@
-﻿namespace Passbook.Generator.Tags;
+﻿using System;
 
-/// <summary>
-/// The date and time the event starts. Use this key for any type of event ticket.
-/// </summary>
-/// <param name="value">ISO 8601 date as string</param>
-public class EventStartDate(string value) : SemanticTagBaseValue("eventStartDate", value)
+namespace Passbook.Generator.Tags;
+
+public class EventStartDate : SemanticTagBaseValue
 {
+    /// <summary>
+    /// The date and time the event starts. Use this key for any type of event ticket.
+    /// </summary>
+    /// <param name="value">ISO 8601 date as string</param>
+    public EventStartDate(string value) : base("eventStartDate", value)
+    {
+    }
+
+    /// <summary>
+    /// The date and time the event starts. Use this key for any type of event ticket.
+    /// </summary>
+    /// <param name="value">date and time with time zone</param>
+    public EventStartDate(DateTimeOffset value) : base("eventStartDate", value)
+    {
+    }
 }
